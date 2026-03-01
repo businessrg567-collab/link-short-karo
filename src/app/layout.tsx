23da308c1d7311079e5165096a66fa5306a044d6
Radhika Gupta <businessrg567@gmail.com>
2026-03-01 09:55:14 +0530

    Add Google AdSense verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7882735878066749" crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.variable} suppressHydrationWarning>
         <Navbar />
         {children}
